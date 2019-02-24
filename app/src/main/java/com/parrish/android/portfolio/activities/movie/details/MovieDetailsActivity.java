a7e65c2e924fc6b25f5f21e12305b2545bbeeab9
Terry Parrish <tparri2@emory.edu>
2019-02-24 09:03:53 -0500

    updated dimens.xml

--- app/src/main/java/com/parrish/android/portfolio/activities/movie/details/MovieDetailsActivity.java
+++ app/src/main/java/com/parrish/android/portfolio/activities/movie/details/MovieDetailsActivity.java
@@ -1,16 +1,12 @@
 package com.parrish.android.portfolio.activities.movie.details;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.PersistableBundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
